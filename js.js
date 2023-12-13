c0635c8fb3ade16338ee6f12855e0d97177b2bc9
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-12-13 10:00:22 -0600

    progresse, responsive site 50%

--- js.js
+++ js.js
@@ -50,17 +50,50 @@ $(".image-container").slick({
 
 var counterBtn = document.getElementById("counterBtn");
 var counterValue = document.getElementById("counterValue");
+var addToCartBtn = document.getElementById("addToCartBtn");
+var cartAmount = document.getElementById("cartAmount");
 
-    var contador = 0;
-    counterValue.textContent = contador;
+var contador = 0; // Inicializamos el contador en 0
 
-    counterBtn.addEventListener("click", function(event) {
+counterValue.textContent = contador;
+
+counterBtn.addEventListener("click", function (event) {
     if (event.target.classList.contains("plus-icon")) {
         contador++;
     } else if (event.target.classList.contains("minus-icon")) {
         contador = Math.max(0, contador - 1);
     }
     counterValue.textContent = contador;
+});
+
+addToCartBtn.addEventListener("click", function () {
+
+    // Actualizar el contenido y el estilo del elemento cart-amount
+    cartAmount.textContent = contador;
+
+    // Ocultar el elemento cuando el contador es cero
+    cartAmount.style.display = contador === 0 ? "none" : "inline"; // O el valor que necesites
+});
+
+
+
+
+
+
+
+var cartModal = document.getElementById('cart-modal');
+    var shopCart = document.getElementById('shop-cart');
+
+    // Toggle para mostrar/ocultar el modal al hacer clic en el carrito
+    shopCart.addEventListener('click', function () {
+        cartModal.classList.toggle('active');
+    });
+
+    // Ocultar el modal al hacer clic fuera de él
+    window.addEventListener('click', function (event) {
+        if (!cartModal.contains(event.target) && event.target !== shopCart) {
+            cartModal.classList.remove('active');
+        }
     });
 
 
